28c08555b4f7827fedf6be7a98418ae165361bee
clinturbin <clinturbin@gmail.com>
2018-07-23 20:36:02 -0400

    Added event listeners to show placeholder Lightbox.

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
-var images = [ 'images/img1.jpg', 'images/img2.jpg', 'images/img3.jpg', 'images/img4.jpg', 'images/img5.jpg', 'images/img6.jpg' ];
+var images = [ 'images/img1.jpg', 'images/img2.jpg', 'images/img3.jpg', 
+                'images/img4.jpg', 'images/img5.jpg', 'images/img6.jpg', 
+                'images/img7.jpg', 'images/img8.jpg' ];
 
-// Get the container where all the image thumbnails will appear
+
+var lightbox = document.querySelector('.lightbox_screen');
 var imageThumbnails = document.querySelector('.image_thumbnails');
 
+// use (var image in images) instead
 for (var i = 0; i < images.length; i++) {
-    // set a variable for the current image
     var image = images[i];
-    // create an element for the image
     var newImage = document.createElement('img');
-    // set attribute for image element
     newImage.setAttribute('src', image);
-    // add class to image
     newImage.classList.add('image');
 
-    // create a div element to serve as container for image
     var imageContainer = document.createElement('div');
-    // add class to image container
     imageContainer.classList.add('img_container');
-    // add image to image container
     imageContainer.appendChild(newImage);
 
-    // add image container to container for all thumbnails
     imageThumbnails.appendChild(imageContainer);
-
 }
+
+var imageContainers = document.querySelectorAll('.img_container');
+var showLightBox = function () {
+    lightbox.classList.remove('hide');
+};
+
+for (var container of imageContainers) {
+    container.addEventListener('click', showLightBox);    
+}
+
